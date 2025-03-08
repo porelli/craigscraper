72e464d81a84e0199fa5462cfaf9da378b8f038e
Michele Porelli <linux571@gmail.com>
2025-03-08 08:54:26 -0800

    feat(docker) prevent container from exiting prematurely

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ COPY . .
 
 RUN pip3 install -r requirements.txt
 
-CMD ["python3", "main.py"]
+CMD ["bash", "run.sh"]
---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# To be used for docker only
+
+crawler() {
+    python3 /app/main.py
+}
+
+trap 'kill $(jobs -p)' EXIT; until crawler & wait; do
+    echo "Scraper crashed with exit code $?. Respawning.." >&2
+    sleep 1
+done
